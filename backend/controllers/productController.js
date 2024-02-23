5a5316117e283e0026d3d7cb683a48cc7c9f7a45
kedarsingh10 <kedarchhetri10@gmail.com>
2024-02-23 18:27:06 +0530

    feat: Add product - routes, controller, and middleware feat: Implement data seeding and deletion functionaliy

---
+++ backend/controllers/productController.js
@@ -0,0 +1,30 @@
+import Product from '../models/productModel.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    if(products){
+        return res.json(products);
+    }
+    res.status(404);
+    throw new Error('Resource Not Found');
+});
+
+// @desc    Fetch a products
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if(product){
+        return res.json(product);
+    }
+    
+    res.status(404);
+    throw new Error('Resource not found');
+});
+
+export { getProducts, getProductById};
